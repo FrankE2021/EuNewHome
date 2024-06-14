a6083b82f15022dd7eb9457597738f33d1609fbe
Soy Frank <ernestinversion@gmail.com>
2024-06-14 12:25:24 -0300

    Updating Article and Portafolio

--- src/pages/Portafolio.jsx
+++ src/pages/Portafolio.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from "../style";
 import { Footer, Navbar } from "../components";
-import Projects from '../components/Projects';
+import { projectsData } from "../constants";
+import Article from '../components/Article';
+import { blog05 } from '../components/blog/imports';
+import { menuBras ,euHome, ahorcado,tareas } from './importsPortafolio'
 
 const Portafolio = () => {
 
@@ -22,10 +25,43 @@ const Portafolio = () => {
 
    {/*  ==================== Contenido ====================*/}
 
-    <div className='ml-4 mr-4 '>
-      <div >
-        <Projects handleButtonClick={handleButtonClick}/>       
-      </div>    
+    <div className='ml-4 mr-4 '>  
+      <div className="rounded-[20px] flex-1 grid grid-cols-1 gap-8 md:grid-cols-3 lg:grid-cols-4">
+        <Article 
+          imgUrl={menuBras}
+          date="Jun 20, 2024" 
+          title={projectsData[0]?.title} 
+          text={projectsData[0]?.description} 
+          link={projectsData[0]?.link} 
+          handleButtonClick={(link) => handleButtonClick(link)} />
+
+        <Article 
+          imgUrl={euHome} 
+          date="Dec 12, 2022" 
+          title={projectsData[1]?.title} 
+          text={projectsData[1]?.description} 
+          link={projectsData[1]?.link} 
+          handleButtonClick={(link) => handleButtonClick(link)} 
+          />
+
+        <Article 
+          imgUrl={ahorcado} 
+          date="Mar 10, 2024"
+          title={projectsData[2]?.title}  
+          text={projectsData[2]?.description}
+          link={projectsData[2]?.link}
+          handleButtonClick={(link) => handleButtonClick(link)} 
+          />
+
+        <Article 
+          imgUrl={tareas} 
+          date="May 15, 2024" 
+          title={projectsData[3]?.title}
+          text={projectsData[3]?.description}
+          link={projectsData[3]?.link}
+          handleButtonClick={(link) => handleButtonClick(link)} 
+          />
+      </div>  
     </div>
 
 
---
+++ src/pages/importsPortafolio.js
@@ -0,0 +1,11 @@
+import menuBras from '../assets/menuBras.png';
+import euHome from '../assets/euHome.png';
+import ahorcado from '../assets/ahorcado.png';
+import tareas from '../assets/tareas.png';
+
+export {
+  menuBras,
+  euHome,
+  ahorcado,
+  tareas
+};
